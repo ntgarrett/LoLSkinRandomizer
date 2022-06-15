02e3c0a16eb7b14362ee1228016bd97bc6309e49
Nicholas Garrett <garrett.nick.96@gmail.com>
2022-06-14 17:33:45 -0700

    Adds a header component and implements redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+
+import Header from './components/Header';
+import { getVersionNumber } from './redux/version/versionSlice';
+import { getChampions } from './redux/champions/championsSlice';
 import './App.css';
 
 function App() {
+
+  const {versionNumber, loading} = useSelector((state) => state.version);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getVersionNumber());
+    localStorage.setItem('currentPatch', versionNumber);
+    dispatch(getChampions());
+  }, [dispatch, versionNumber]);
+
   return (
     <div className="App">
-      <header>
-        LoL Skin Randomizer
-      </header>
+      <Header patch={localStorage.getItem('currentPatch')}/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+function Header({patch}) {
+
+  return (
+    <div>
+      <h3>LoL Skin Randomizer - {patch}</h3>
+    </div>
+  );
+}
+
+export default Header;
