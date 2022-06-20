bd9565311ebadc82cb37e135e7539b682da8e428
Nicholas Garrett <garrett.nick.96@gmail.com>
2022-06-20 13:05:39 -0700

    Adds champion search bar container/component

---
+++ src/components/ChampionFilter.js
@@ -0,0 +1,32 @@
+import { TextField, Box, Typography, ButtonBase } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const ChampionFilter = (props) => {
+  return (
+    <div className='filterbar'>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <ButtonBase
+          onClick={props.onSubmitSearch}
+          className='searchicon'
+          disabled={props.searchValue.length === 0}
+        >
+          <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        </ButtonBase>
+        <TextField 
+          id="champion-search"
+          label="Search champions"
+          variant="standard"
+          onChange={props.onChange}
+          onKeyDown={props.handleKeyPress}
+        />
+      </Box>
+      <div>
+        <Typography>
+
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default ChampionFilter;
---
+++ src/containers/ChampionFilterContainer.js
@@ -0,0 +1,43 @@
+//import { useState } from 'react';
+
+import ChampionFilter from "../components/ChampionFilter";
+
+const ChampionFilterContainer = (props) => {
+  //const [value, setValue] = useState('');
+
+  const { setFilteredChampionList, allChampions, setSelectedChampion, searchValue, setSearchValue } = props;
+
+  const filterListOfChampions = (val) => {
+    let filteredList = allChampions.filter((champ) => champ.name.toLowerCase().includes(val.toLowerCase()));
+
+    setFilteredChampionList(filteredList);
+  }
+  
+  const onChange = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const onSubmitSearch = () => {
+    filterListOfChampions(searchValue);
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      filterListOfChampions(searchValue);
+    } else {
+      setSearchValue(e.target.value);
+    }
+  }
+
+  return (
+    <ChampionFilter 
+      onChange={onChange}
+      onSubmitSearch={onSubmitSearch}
+      handleKeyPress={handleKeyPress}
+      searchValue={searchValue}
+      setSelectedChampion={setSelectedChampion}
+    />
+  );
+};
+
+export default ChampionFilterContainer;
