e46fd5e3d7cda42e39bc73af61d627e3aac7a063
Nicholas Garrett <garrett.nick.96@gmail.com>
2022-09-21 00:12:13 -0700

    Adds packaging/building/distribution

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,10 @@
+const { app, BrowserWindow, protocol } = require('electron')
 const path = require('path')
-
-const { app, BrowserWindow } = require('electron')
-const isDev = require('electron-is-dev')
-
-const electronReload = require('electron-reload')(__dirname)
+const url = require('url')
 
 function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
     webPreferences: {
@@ -16,25 +13,49 @@ function createWindow() {
     resizable: false,
     maximizable: false,
     icon: path.join(__dirname, 'favicon.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    }
   })
 
-  win.menuBarVisible = false
+  mainWindow.menuBarVisible = false
 
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`,
+  const appURL =
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+
+  mainWindow.loadURL(appURL)
+}
+
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    'file',
+    (request, callback) => {
+      const url = request.url.substr(8)
+      callback({ path: path.normalize(`${__dirname}/${url}`) })
+    },
+    (error) => {
+      if (error) console.error('Failed to register protocol')
+    }
   )
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' })
-  }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(() => { 
+  createWindow()
+  setupLocalFilesNormalizerProxy()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -42,11 +63,5 @@ app.whenReady().then(createWindow)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
   }
 })
